1b38dbd14e7a5e320956079b49356093e3baa845
Guilherme907 <guilherme.meireles567@gmail.com>
2026-03-03 10:00:10 +0100

    test merge
    cours3

--- src/texte.py
+++ src/texte.py
@@ -1,21 +1,41 @@
+from collections import Counter
+
 class Texte:
-    def __init__(self, titre: str, auteur: str, annee: int, contenu: str):
-        self.titre = titre
+    def __init__(self, titre: str, auteur: str, contenu: str):
+        self._titre = titre
         self.auteur = auteur
-        self.annee = annee
         self.contenu = contenu
 
+    @property
+    def titre(self) -> str:
+        return self._titre
+    @titre.setter
+    def titre(self, nouveau: str) -> None:
+        if not nouveau.strip():
+            raise ValueError("Le titre est vide, ou il y a des espace au début/à la fin")
+        self._titre = nouveau.strip()
+
+
     def nombre_mots(self) -> int:
-        return len(self.contenu.split())
-    def mots_uniques(self) -> set[str]:
-        return set(self.contenu.split())
+        return sum(len(texte.split()) for texte in self.contenu)
+
+    def mots_uniques(self) -> set[str]: #set -> que les éléments uniques
+        cnt = Counter(self.contenu.split())
+        mots_unic = []
+        for word in cnt:
+            if cnt[word] == 1:
+                mots_unic.append(word)
+        return set(mots_unic)
+    
     def frequences(self) -> dict[str, int]:
-        freqs = {}
-        for mot in self.contenu.split():
-            if mot in freqs:
-                freqs[mot] += 1
-            else:
-                freqs[mot] = 1
-        return freqs
+        cnt = Counter(self.contenu.split())
+        return cnt
+    
 
+if __name__ == "__main__":
+    texte_test = Texte("Titre", "Moi", "Un texte quelconque oui oui")
+    print(texte_test.titre)
+    #texte_test.titre = ""
+    #print(texte_test.mots_uniques())
 
+    print(texte_test.frequences())
